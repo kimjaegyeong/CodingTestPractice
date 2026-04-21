0b15c99b6ebf3388cce5be32d56645a000f3fb37
kimjaegyeong <50646904+kimjaegyeong@users.noreply.github.com>
2026-04-22 01:09:17 +0900

    [Bronze III] Title: 정수의 개수, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(",");
+		
+		int n=0;
+        HashSet<String> set = new HashSet<String>();
+        
+        System.out.println(input.length);
+	}
+}
